a8ab31d6344611f6638927f78197d4eecd97789c
viktor Xu <xulai1001@gmail.com>
2020-04-01 21:32:15 +0800

    fix

--- resources/akdata.js
+++ resources/akdata.js
@@ -17,7 +17,7 @@ window.AKDATA = {
     for( let i=0;i<paths.length;i++) {
       if ( paths[i].endsWith('.json') ){
         let name = paths[i].split('/').pop().replace('.json', '');
-        let path = `https://cdn.jsdelivr.net/gh/xulai1001/akdata@${akVersion}/resources/gamedata/${paths[i].toLowerCase()}`;
+        let path = `https://cdn.jsdelivr.net/gh/xulai1001/akdata@${window.AKDATA.akVersion}/resources/gamedata/${paths[i].toLowerCase()}`;
         //let path = `../resources/gamedata/${paths[i].toLowerCase()}`;
           
         paths[i] = loadJSON(path, data => AKDATA.Data[name] = data);
